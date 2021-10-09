6f3bb72573842a27aa8e56ae60f1629b3c6046a1
navneetdavang <navneetdavang@gmail.com>
2021-10-09 18:47:16 +0530

    JDBC class

---
+++ Demo2/src/com/jdbc/main/JDBC.java
@@ -0,0 +1,10 @@
+package com.jdbc.main;
+
+public class JDBC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("THis is JDBC class");
+	}
+
+}
